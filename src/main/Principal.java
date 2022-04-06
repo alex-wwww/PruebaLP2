712b59eab7b6420a1064bef16c66dcf2a2375c9d
alex-wwww <a20185462@pucp.edu.pe>
2022-04-06 16:52:53 -0500

    Prueba numero 2

--- src/main/Principal.java
+++ src/main/Principal.java
@@ -11,6 +11,6 @@ package main;
  */
 public class Principal {
     public static void main(String[] args ) {
-        System.out.println("Hola mundo...");
+        System.out.println("Hola mundo... prueba2");
     }
 }
